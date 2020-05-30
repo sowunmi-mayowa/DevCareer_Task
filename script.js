07c6d42dd67e580302a41ca039f4726ef48d27e3
mayowa <smayowa176gmail.com>
2020-05-30 17:34:18 +0100

    fixed pasword length value

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function formValidation(e){
     if(firstName.value =="" || lastName.value =="" || email.value =="" || password=="" || confirmPassword ==""){
         let msgElem = document.createElement("div");
         msgElem.textContent = "All Fields Are Mandatory!...";
-        msgElem.classList.add("alert-danger", "form-control");
+        msgElem.classList.add("alert-danger","text-center");
         inputField[0].classList.add("border-danger");
         inputField[1].classList.add("border-danger");
         inputField[2].classList.add("border-danger");
@@ -31,7 +31,7 @@ function formValidation(e){
     else if(password.value=== "" && confirmPassword.value ===""){
         let msgElem = document.createElement("div");
         msgElem.textContent = "passwords are required!...";
-        msgElem.classList.add("alert-danger", "form-control");
+        msgElem.classList.add("alert-danger", "text-center");
         inputField[3].classList.add("border-danger");
         inputField[4].classList.add("border-danger");
         parentMsg.appendChild(msgElem);
@@ -42,15 +42,14 @@ function formValidation(e){
     else if(password.value !== confirmPassword.value){
         let msgElem = document.createElement("div");
         msgElem.textContent = "Password Mismatch.. Try Again!";
-        msgElem.classList.add("alert-warning", "form-control");
+        msgElem.classList.add("alert-warning", "text-center");
         parentMsg.appendChild(msgElem)
         setTimeout(() => msgElem.remove(), 5000)
     }
-    else if(password.maxLength < 8 && confirmPassword.maxLength
-         < 8){
+    else if(password.value.length < 8 && confirmPassword.value.length < 8){
         let msgElem = document.createElement("div");
         msgElem.textContent = "passwords should contain more than 8 character";
-        msgElem.classList.add("alert-danger", "form-control");
+        msgElem.classList.add("alert-danger", "text-center");
         inputField[3].classList.add("border-danger");
         inputField[4].classList.add("border-danger");
         parentMsg.appendChild(msgElem);
@@ -61,7 +60,7 @@ function formValidation(e){
     else{
         let msgElem = document.createElement("div");
         msgElem.textContent = "Your Account Has Been Created Successfully...";
-        msgElem.classList.add("alert-success", "form-control");
+        msgElem.classList.add("alert-success", "text-center");
         parentMsg.appendChild(msgElem)
         setTimeout(() => msgElem.remove(), 5000)
         inputField[0].classList.add("border-success");
